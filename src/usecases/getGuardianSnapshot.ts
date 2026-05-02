3d54596aa173686eb072a884d3647803b8a79f8d
nusratulloh0110-png <nusratulloh0110@gmail.com>
2026-05-02 14:19:55 +0500

    Delete src directory

--- src/usecases/getGuardianSnapshot.ts
+++
@@ -1,5 +0,0 @@
-import type { PortfolioRepository, PortfolioSnapshot } from "../domain/portfolio/types";
-
-export const getGuardianSnapshot = (repository: PortfolioRepository): Promise<PortfolioSnapshot> => {
-  return repository.getSnapshot();
-};
